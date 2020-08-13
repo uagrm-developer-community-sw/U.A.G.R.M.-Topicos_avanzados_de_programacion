5ca08fe30d792364bd848b18f44ac9305c9f79d3
jhasmany_jhunnior@outlook.com <jhasmany>
2020-08-13 04:38:02 -0400

    suma dialogofow

--- webhooks/empresasegurosbot.php
+++ webhooks/empresasegurosbot.php
@@ -1,5 +1,14 @@
 <?php 
 include_once "../somosioticos/somosioticos_dialogflow.php";
 credenciales('empresasegurosbot','123456789');
-echo "bienvenidos al webhook";
+
+
+if (intent_recibido("calculadora")){
+    $valor1 = obtener_variables()['numero1'];
+    $valor2 = obtener_variables()['numero2'];
+    $resultado = $valor1 + $valor2;
+    enviar_texto("Luego de sumar los valores te digo que el resultado es $resultado");
+}
+
+
 ?>
