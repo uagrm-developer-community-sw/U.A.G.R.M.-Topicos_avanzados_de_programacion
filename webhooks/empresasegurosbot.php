d1b42385e1cc60901b6c3bcd81ef9edd6aa8afc5
jhasmany_jhunnior@outlook.com <jhasmany>
2020-08-15 14:09:32 -0400

    texto servicios

--- webhooks/empresasegurosbot.php
+++ webhooks/empresasegurosbot.php
@@ -10,5 +10,13 @@ if (intent_recibido("calculadora")){
     enviar_texto("Luego de sumar los valores te digo que el resultado es $resultado");
 }
 
-
+if (intent_recibido("Ofrecer_Servicios")){
+    enviar_texto("Nuestro Servicios:    
+    \n* Asistencia de grúa en casos de siniestro o avería
+    \n* Auxilio mecánico en casos de avería o accidente.
+    \n* Auto Reemplazo en casos de siniestro.
+    \n* Extraterritorialidad.
+    \n* Atención las 24 horas del día, los 365 dás del año.
+    \n* Servicio de procuraduría personal y especializado.");
+}
 ?>
