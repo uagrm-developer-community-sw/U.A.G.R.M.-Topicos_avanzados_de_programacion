302be5a7543258961375b151614a9f92c901ed6b
jhasmany_jhunnior@outlook.com <jhasmany>
2020-08-30 17:27:31 -0400

    actaulizacion 1

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="egm_phpcrud";
+$servername="us-cdbr-east-02.cleardb.com";
+$username="bbf45313cdcae5";
+$password="8aa480ec";
+$dbname="heroku_9589008d54b8835";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
