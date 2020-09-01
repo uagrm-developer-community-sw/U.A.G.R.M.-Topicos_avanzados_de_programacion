0f8a4df8518d403989c3bb5099eb316de2200252
jhasmany_jhunnior@outlook.com <jhasmany>
2020-08-31 23:24:05 -0400

    bd imprimir 1

--- webhooks/sautomotorbot.php
+++ webhooks/sautomotorbot.php
@@ -8,15 +8,16 @@
 
     if($result)
     {
-    while($row = mysqli_fetch_assoc($result)){
+        while($row = mysqli_fetch_assoc($result)){
 
-        echo $row['id'];
-        echo $row['firstname'];
-        echo $row['lastname'];
-        echo $row['address'];
-        echo $row['skills'];
-        echo $row['designation'];
+            echo $row['id'];
+            echo $row['firstname'];
+            echo $row['lastname'];
+            echo $row['address'];
+            echo $row['skills'];
+            echo $row['designation'];
         
+        }
     }
 ?>
 
