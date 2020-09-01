8eb36f30059b2ae5b2e89c18c2303829df9ca3db
jhasmany_jhunnior@outlook.com <jhasmany>
2020-09-01 09:29:30 -0400

    bd imprimir 5

--- webhooks/sautomotorbot.php
+++ webhooks/sautomotorbot.php
@@ -1,27 +1,28 @@
 <?php
-  include('../connection.php');
+
   include('libreria_developer/sautomotorbot.php');
   credenciales ('segurosautomotorbot','123456789');
+  include('../connection.php');
 
+    if(intent_recibido("datos_cliente")){
+        $sql = "SELECT * FROM tbl_record";
+        $result = mysqli_query($conn, $sql);
+        if($result)
+        {
+            while($row = mysqli_fetch_assoc($result)){
 
-
-    $sql = "SELECT * FROM tbl_record";
-    $result = mysqli_query($conn, $sql);
-
-    if($result)
-    {
-        while($row = mysqli_fetch_assoc($result)){
-
-            echo $row['id'];
-            echo $row['firstname'];
-            echo $row['lastname'];
-            echo $row['address'];
-            echo $row['skills'];
-            echo $row['designation'];
-        
+                $_id = $row['id'];
+                $_firstname = $row['firstname'];
+                $_lastname = $row['lastname'];
+                $_address = $row['address'];
+                $_skills = $row['skills'];
+                $_designation = $row['designation'];
+                enviar_texto("Nombre: $_firstname Apellido: $_lastname Direccion: $_address Especialidad: $_skills Fecha Ingeso: $_designation");
         }
-    }else{
-        echo "<script> alert('No Record Found');</script>";
+        }else{
+            echo "<script> alert('No Record Found');</script>";
+        }
+            
     }
 ?>
 
